98b5627b8713f10e1a0fb72afe4b93f143580f31
Iliana Todorova <ilia20na23janav@gmail.com>
2024-06-17 20:37:42 +0200

    initial commit

---
+++ src/main/java/bg/softuni/libraria/LibrariaApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.libraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/libraria/LibrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.libraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
