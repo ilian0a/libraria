d3574d17dfa5b6b1f65017162ba301302b245bfc
Iliana Todorova <ilia20na23janav@gmail.com>
2024-07-07 14:47:26 +0200

    Filled model layer and modified landing page / buttons added

--- src/main/java/bg/softuni/libraria/model/UserRegistrationDTO.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.libraria.model;
-
-public record UserRegistrationDTO (String firstName,
-                                   String lastName, String password,
-                                   String email) {
-
-
-
-}
--- src/main/java/bg/softuni/libraria/service/impl/UserServiceImpl.java
+++ src/main/java/bg/softuni/libraria/service/impl/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package bg.softuni.libraria.service.impl;
 
-import bg.softuni.libraria.model.UserRegistrationDTO;
+import bg.softuni.libraria.service.UserRegistrationDTO;
 import bg.softuni.libraria.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Override
-    public void registerUer(UserRegistrationDTO userRegistrationDTO) {
+    public void registerUser(UserRegistrationDTO userRegistrationDTO) {
         // TODO
         System.out.println("User registered: " + userRegistrationDTO);
     }
