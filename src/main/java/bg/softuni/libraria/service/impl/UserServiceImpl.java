ba96d244d0e71d4ec3ee952dc72fca8b9ba011b9
Iliana Todorova <ilia20na23janav@gmail.com>
2024-07-07 16:38:24 +0200

    Switched to lombok and filled models

--- src/main/java/bg/softuni/libraria/service/impl/UserServiceImpl.java
+++ src/main/java/bg/softuni/libraria/service/impl/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package bg.softuni.libraria.service.impl;
 
-import bg.softuni.libraria.service.UserRegistrationDTO;
+import bg.softuni.libraria.service.UserRegisterDTO;
 import bg.softuni.libraria.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Override
-    public void registerUser(UserRegistrationDTO userRegistrationDTO) {
+    public void registerUser(UserRegisterDTO userRegistrationDTO) {
         // TODO
         System.out.println("User registered: " + userRegistrationDTO);
     }
--- src/main/java/bg/softuni/libraria/web/UserController.java
+++ src/main/java/bg/softuni/libraria/web/UserController.java
@@ -1,8 +1,51 @@
 package bg.softuni.libraria.web;
 
+import bg.softuni.libraria.service.UserRegisterDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/register")
+    public String viewRegister(Model model) {
+
+        model.addAttribute("registerData", new UserRegisterDTO());
+
+
+        return "register";
+    }
+
+
+
+    @PostMapping("/register")
+    public String doRegister(@Valid UserRegisterDTO userData,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes){
+
+        if (bindingResult.hasErrors()) {
+
+            redirectAttributes.addAttribute("registerData", userData);
+
+            return "register";
+        }
+
+
+        return "redirect:/users/login";
+    }
+
+    @GetMapping("/login")
+    public String viewLogin() {
+
+        return "login";
+    }
+
 
 }
